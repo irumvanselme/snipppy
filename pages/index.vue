dd1f0051572199a0f2a812b9548621f2c4cecef3
irumvanselme <andesanselme@gmail.com>
2022-09-20 21:06:56 +0200

    feat: change tabsize to 4

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <h1>Hello this is is irumva anselme 21</h1>
-    <div class="hello text-blue-600">hello</div>
-  </div>
+    <div>
+        <h1>Hello this is is irumva anselme 21</h1>
+        <div class="hello text-blue-600">hello</div>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+    name: 'IndexPage',
 })
 </script>
